b4af68663261db6ff678a849aaac35bf74822e9e
Nico <nicolas.schomberg@gmail.com>
2017-08-11 15:39:05 -0700

    Tweak

--- src/components/Task/Avatar.js
+++ src/components/Task/Avatar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const Avatar = ({ user }) => (
   <span className="slds-avatar slds-avatar--medium slds-avatar_circle">
     {user.picture
-      ? <img alt={user.name} src={user.picture} title="User avatar" />
+      ? <img alt={user.name} src={user.picture} title={user.name} />
       : <abbr className="slds-avatar__initials slds-icon-standard-user" title={user.name}>{user.initials}</abbr>
     }
   </span>
